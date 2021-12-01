dc85754717603d21d1ce19539dbffe5f7cec110a
ManishShah120 <mkshah141@gmail.com>
2021-12-01 22:25:17 +0530

    Basic django&DRF done

--- blog/tests.py
+++ blog/tests.py
@@ -1,1 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from blog.models import Post, Category
+
+
+class Test_Create_Post(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='django')
+        testuser1 = User.objects.create(username='tester', password='test123')
+        test_post = Post.objects.create(category_id=1, title='Test Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+
+    def test_blog_content(self):
+        post = Post.postobjects.get(id=1)
+        cat = Category.objects.get(id=1)
+        author = f'{post.author}'
+        excerpt = f'{post.excerpt}'
+        title = f'{post.title}'
+        content = f'{post.content}'
+        status = f'{post.status}'
+        self.assertEqual(author, 'tester')
+        self.assertEqual(title, 'Test Post Title')
+        self.assertEqual(content, 'Post Content')
+        self.assertEqual(status, 'published')
+        self.assertEqual(cat.name, 'django')
+        self.assertEqual(excerpt, 'Post Excerpt')
+        self.assertEqual(str(post), 'Test Post Title')
+        self.assertEqual(str(cat), 'django')
--- core/settings.py
+++ core/settings.py
@@ -69,3 +69,9 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
